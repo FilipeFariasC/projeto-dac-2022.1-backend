c9176a41e9db12cf6504cd89458b6c83d27d088d
Filipe Farias <filipe.farias.chagas@protonmail.com>
2022-04-13 19:22:38 -0300

    criacao da entidade usuario

---
+++ src/main/java/br/edu/ifpb/dac/groupd/User.java
@@ -0,0 +1,66 @@
+package br.edu.ifpb.dac.groupd;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="t_user")
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6820520515993917458L;
+	
+	
+	@Id
+	@Column(name="user_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@NotEmpty
+	@NotBlank
+	@Email
+	private String email;
+	
+	@NotNull
+	@NotEmpty
+	@NotBlank
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
