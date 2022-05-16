68ad850aff1397a5718b982944fbda4e45d81a60
Eliana Melo <elianamacielmelo@gmail.com>
2022-05-16 15:49:57 -0300

    Alteracao na BraceletTest e criacao AlarmServiceTest

---
+++ src/test/java/br/edu/ifpb/dac/groupd/tests/unit/AlarmServiceTest.java
@@ -0,0 +1,51 @@
+package br.edu.ifpb.dac.groupd.tests.unit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+
+import br.edu.ifpb.dac.groupd.model.Alarm;
+import br.edu.ifpb.dac.groupd.repository.AlarmRepository;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+@Testable
+@DisplayName("AlarmService")
+public class AlarmServiceTest {
+	
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
+	@Autowired
+	private AlarmRepository alarmRepository; 
+	
+	@Test
+	@DisplayName("return list Alarm")
+	void listFind() {
+		Alarm saveAlrm = alarmRepository.save(new Alarm());
+		
+		List<Alarm> alarms = testRestTemplate.exchange("/search", HttpMethod.GET,
+				null, new ParameterizedTypeReference<List<Alarm>>() {
+				}).getBody();
+		
+		Assertions.assertThat(alarms).isNotNull().isNotEmpty().hasSize(1);
+		
+		Assertions.assertThat(alarms.get(0).getClass()).isEqualTo(saveAlrm.getClass());
+		
+	}
+	
+	
+	
+}
--- src/test/java/br/edu/ifpb/dac/groupd/tests/unit/BraceletTests.java
+++ src/test/java/br/edu/ifpb/dac/groupd/tests/unit/BraceletTests.java
@@ -2,7 +2,16 @@ package br.edu.ifpb.dac.groupd.tests.unit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
+import java.time.LocalTime;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -13,19 +22,29 @@ import javax.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.commons.annotation.Testable;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.edu.ifpb.dac.groupd.model.Bracelet;
+import br.edu.ifpb.dac.groupd.model.Coordinate;
+import br.edu.ifpb.dac.groupd.model.Fence;
+import br.edu.ifpb.dac.groupd.model.Location;
 
 @Testable
 @DisplayName("Bracelet")
 public class BraceletTests {
 	
 	private Bracelet bracelet;
+	@Mock
+	private Fence fence;
+	@Mock
+	private Coordinate coordinate;
 	
 	private Set<ConstraintViolation<Bracelet>> violations;
 	
@@ -75,4 +94,71 @@ public class BraceletTests {
 		violations = validator.validateProperty(bracelet, "name");
 		assertEquals(0, violations.size(), () -> "Valid name" );
 	}
+	
+	@Test
+	void fence() {
+		fence = new Fence();
+		fence.setId(null);
+		fence.setBracelets(null);
+		
+		coordinate = new Coordinate();
+		coordinate.setLatitude(-8.07439);
+		coordinate.setLongitude(-37.2646);
+		
+		fence.setCoordinate(coordinate);
+		fence.setRadius(10.00);
+		fence.setStartTime(LocalTime.now());
+		fence.setFinishTime(LocalTime.now().plusHours(1));
+		
+		openMocks(this);
+		
+		bracelet.setMonitor(fence);
+		
+		assertEquals(fence, bracelet.getMonitor());
+		
+	}
+	
+	/**
+	@Test
+	void fenceMock() {
+		fence.addBracelet(bracelet);
+		verify(fence.getBracelets()).add(bracelet);
+		
+	}
+	**/
+	
+	@Test
+	void testAddFence() {
+		
+	}
+	
+	@Test
+	void Location() {
+		Location location = new Location();
+		location.setId(null);
+		location.setBracelet(bracelet);
+		
+		coordinate = new Coordinate();
+		coordinate.setLatitude(-8.07439);
+		coordinate.setLongitude(-37.2646);
+		
+		
+		location.setCoordinate(coordinate);
+		location.setCreationDate(null);
+		
+		List<Location> list = new LinkedList<Location>();
+		List<Location> locations = spy(list);
+		locations.add(location);
+		
+		verify(locations).add(location);
+		
+		when(locations.size()).thenReturn(1);
+		
+		violations = validator.validateProperty(bracelet, "locations");
+		assertEquals(0, violations.size(), () -> "Invalid location" );
+	}
+	
+	
+	
+		
 }
