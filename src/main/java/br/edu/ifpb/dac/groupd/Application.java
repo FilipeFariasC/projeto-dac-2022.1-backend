a92cf8e2139bbb61991352c8273aee07b7f333d2
Eliana Melo <elianamacielmelo@gmail.com>
2022-05-24 17:37:54 -0300

    modificação na class Application, adicionando Cors Policy

--- src/main/java/br/edu/ifpb/dac/groupd/Application.java
+++ src/main/java/br/edu/ifpb/dac/groupd/Application.java
@@ -3,11 +3,20 @@ package br.edu.ifpb.dac.groupd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class Application {
+@EnableWebMvc
+public class Application implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
 	}
 }
